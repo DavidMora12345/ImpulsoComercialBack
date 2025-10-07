a571392e6b8ed0d3ddf125a437fb57cd00f69c86
DavidMora12345 <davidestebanmora7@gmail.com>
2025-10-07 10:12:56 -0500

    initial ControllerStatus

---
+++ PComercialTest/src/main/java/Controller/ControllerStatus.java
@@ -0,0 +1,15 @@
+package com.PComercial.PComercialTest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerStatus {
+
+    @GetMapping("/api/status")
+    public ResponseEntity<String> status() {
+        return ResponseEntity.ok("OK!");
+    }
+
+}
