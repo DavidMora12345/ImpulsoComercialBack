9e00f988fbb162822aa6c83f688e12052ac66ebe
DavidMora12345 <davidestebanmora7@gmail.com>
2025-10-07 09:53:09 -0500

    initial commit

---
+++ PComercialTest/src/main/java/com/PComercial/PComercialTest/PComercialTestApplication.java
@@ -0,0 +1,13 @@
+package com.PComercial.PComercialTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PComercialTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PComercialTestApplication.class, args);
+	}
+
+}
---
+++ PComercialTest/src/test/java/com/PComercial/PComercialTest/PComercialTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PComercial.PComercialTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PComercialTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
